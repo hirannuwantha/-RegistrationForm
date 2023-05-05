7f1fbea61cceb7ce9b97b63d1d4b6c576fbcb8f7
hirannuwantha <124451651+hirannuwantha@users.noreply.github.com>
2023-05-05 14:08:13 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Form from "./components/form";
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="card">
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
